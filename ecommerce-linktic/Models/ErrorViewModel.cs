8e959c4c6b0bef2b4b70ac5f6987e757d965741a
Lisandro <lisandro.zubieta@infotegra.com>
2024-10-01 19:36:39 -0500

    Agregar archivos de proyecto.

---
+++ ecommerce-linktic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecommerce_linktic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
